15759131fbb92e8d7c18991e3d78ed0646d1d072
Arthur Martins <mfarthurr@gmail.com>
2023-04-05 17:23:56 -0700

    Initial Commit

---
+++ swift_stanford/swift_stanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_stanfordApp.swift
+//  swift_stanford
+//
+//  Created by Arthur Martins on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_stanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
